bfe19d809cba6b87175fd34c8c366629957bda81
Monty Gole <montygole@gmail.com>
2023-03-14 22:26:30 -0400

    fixed wiki finding code

--- Assignment-2/src/entity_wikis.py
+++ Assignment-2/src/entity_wikis.py
@@ -2,11 +2,14 @@ from entity_analysis import extract_entities, entity_frequency
 import fandom
 from tqdm import tqdm
 def get_wikis(entity_list):
+    fandom.set_wiki("gameofthrones")
     wikis = []
-    for x in tqdm(entity_list):
+    for ind, x in entity_list.iterrows():
+        print(x[0])
         try:
-            wiki_page = fandom.page("Runespace")
-            
+            # wiki_page = fandom.page("Runespace")
+            search = fandom.page(x[0])
+            wikis.append(search.plain_text)
         except:
-            print(f"COULDNT RETREIVE WIKI FOR {x}")
-        wikis.append(x)
+            print(f"COULDNT RETREIVE WIKI FOR {x[0]}")
+    return wikis
